e18713be16c86968cfe1f35ab63a25e94b7086a6
Alex Naser <c.a.naser@gmail.com>
2015-07-12 15:00:31 -0400

    adding validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+  validates_presence_of :first_name, :last_name, :trello_token, :trello_public_key
+
   def trello_connect
     thread = Thread.new { yield(trello_client) }
     response = thread.value
