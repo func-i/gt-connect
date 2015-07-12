f746e1958326fb5eea2bef1149499c5c749438a1
Alex Naser <c.a.naser@gmail.com>
2015-07-12 15:00:09 -0400

    adding User#trello_connect

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
 
+  def trello_connect
+    thread = Thread.new { yield(trello_client) }
+    response = thread.value
+    Thread.kill thread
+
+    return response
+  end
+
+  protected
+
+  def trello_client
+    @trello_client ||= Trello::Client.new(developer_public_key: trello_public_key, member_token: trello_token)
+  end
+
 end
